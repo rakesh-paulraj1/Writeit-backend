cb5834987187de64fd552ffa3c4823b19a70de89
rakesh-paulraj <rakeshpaulraj2004@gmail.com>
2024-03-19 21:53:59 +0530

    added 2 routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,62 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
+const app = new Hono<{Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: string
+              }}>();
 
-app.post('/api/v1/user/signup',(c)=>{
-  
+ 
+app.post('/api/v1/user/signup',async (c)=>{
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body=await c.req.json();
+    await prisma.user.create({
+        data: {
+            email: body.email,
+            name: body.name,
+            password: body.password,
+        }
+    })
 })
-app.post('/api/v1/user/signin',(c)=>{
-  
-})
+app.post('/api/v1/user/signin', async (c)=>{
+    
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+    const body= await c.req.json();
+   
+    const user=prisma.user.findUniqueOrThrow({
+        where: {
+            email: body.email,
+            password: body.password
+                },
+            })
+                if(user){
+                    const  token=  sign({ id: (await user).id }, c.env.JWT_SECRET)
+                    return c.json({
+                        jwt:token
+                    })
+                }
+                c.json({
+                    error: "invalid email or password"
+                })
+
+    })
+
+
+
 app.post('/api/v1/blog',(c)=>{
   
 })
 app.put('/api/v1/blog',(c)=>{
+
   
 })
 app.get('/api/v1/blog/:id',(c)=>{
