810b48fb771b9c844431068899056896eda01d2b
rakesh-paulraj <rakeshpaulraj2004@gmail.com>
2024-03-19 20:06:04 +0530

    backend

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+
+app.post('/api/v1/user/signup',(c)=>{
+  
+})
+app.post('/api/v1/user/signin',(c)=>{
+  
+})
+app.post('/api/v1/blog',(c)=>{
+  
+})
+app.put('/api/v1/blog',(c)=>{
+  
+})
+app.get('/api/v1/blog/:id',(c)=>{
+  
+
+})
+
+app.get('/api/v1/blog/bulk',(c)=>{
+  
+})
+
+
+
+
+
+export default app
